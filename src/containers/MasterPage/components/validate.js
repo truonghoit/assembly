7a0ccdaf709e1f4c3be0d8f7f7416b4408222d5b
truong ho <truong.ho.it@gmail.com>
2019-07-08 15:15:14 +0700

    Update some validation for master form input

--- src/containers/MasterPage/components/validate.js
+++ src/containers/MasterPage/components/validate.js
@@ -1,25 +1,29 @@
 const validate = (values) => {
 	const errors = {};
-	if (!values.parent_mas_cd && !values.mas_cd) {
+	if (!values.mas_cd) {
 		errors.mas_cd = 'Mas code field shouldn’t be empty';
 	}
 	if (!values.mas_cd_nm) {
 		errors.mas_cd_nm = 'General Name field shouldn’t be empty';
 	}
-
-	if (!values.cate_cd) {
+	if (!values.cate_cd_nm || (values.cate_cd_nm && values.cate_cd_nm.value == "")) {
 		errors.cate_cd_nm = 'Please select the category';
 	}
-	if (!values.parent_mas_cd && (values.processing_seq != 0)) {
-		errors.processing_seq = 'If parent mas code is empty, process sequence should be 0';
+	if (!values.parent_mas_name || (values.parent_mas_name && values.parent_mas_name.value == "")) {
+		if (values.processing_seq < 0){
+			errors.processing_seq = 'If parent mas code is empty, process sequence should be' +
+				' greater or equal 0';
+		}
 	}
 
-	if (values.parent_mas_cd && (values.processing_seq <= 0)){
+	/*if (values.parent_mas_name && (values.processing_seq <= 0)){
 		errors.processing_seq = 'If parent mas code is selected, process sequence should greater' +
 			' than 0';
-	}
+	}*/
 	if (!values.processing_seq) {
 		errors.processing_seq = 'Process sequence field shouldn’t be empty';
+	} else if (!/^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$/i.test(values.processing_seq)) {
+		errors.processing_seq = 'Process sequence should be number';
 	}
   return errors;
 };
