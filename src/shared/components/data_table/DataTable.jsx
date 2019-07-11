db2f855f0a12c02157d523bf7d90610c5a897c14
Tri Le <lmtri1995@gmail.com>
2019-07-11 11:45:23 +0700

    Add Generic Data Table Component

---
+++ src/shared/components/data_table/DataTable.jsx
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import {ReactTabulator} from "react-tabulator";
+import "../../../scss/component/tabulator_midnight_custom.css"
+import PropTypes from "prop-types";
+
+export default class DataTable extends Component {
+    static propTypes = {
+        columns: PropTypes.arrayOf(
+            PropTypes.oneOfType([
+                PropTypes.shape({
+                    title: PropTypes.string.isRequired,
+                    field: PropTypes.string.isRequired,
+                }),
+                PropTypes.shape({
+                    title: PropTypes.string.isRequired,
+                    columns: PropTypes.arrayOf(
+                        PropTypes.shape({
+                            title: PropTypes.string.isRequired,
+                            field: PropTypes.string.isRequired,
+                        })
+                    ).isRequired,
+                })
+            ])
+        ).isRequired,
+        data: PropTypes.arrayOf(PropTypes.object).isRequired,
+        options: PropTypes.object,
+        onRowClick: PropTypes.func,
+    };
+    static defaultProps = {
+        options: undefined,
+        onRowClick: (mouseEvent, rowComponent) => {
+            console.log("ref table: ", this.ref.table); // this is the Tabulator table instance
+            console.log(`rowClick id: ${rowComponent.getData().id}`, rowComponent, mouseEvent);
+        }
+    };
+
+    constructor(props) {
+        super(props);
+        this.ref = null;
+    }
+
+    render() {
+        let {columns, data, options, onRowClick} = this.props;
+        return (
+            <ReactTabulator
+                ref={ref => this.ref = ref}
+                columns={columns}
+                data={data}
+                options={options}
+                rowClick={onRowClick}
+            />
+        );
+    }
+}
