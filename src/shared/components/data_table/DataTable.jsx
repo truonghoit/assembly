e4efc407da28dea0abc9213114f598e1c8f5b1b3
Tri Le <lmtri1995@gmail.com>
2019-09-23 15:47:07 +0700

    [Data Table Component] style: reformat code

--- src/shared/components/data_table/DataTable.jsx
+++ src/shared/components/data_table/DataTable.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import {ReactTabulator}   from "react-tabulator";
 import "../../../scss/component/tabulator_bootstrap4.min.css";
 import PropTypes          from "prop-types";
+
 var Tabulator = require('tabulator-tables');
 
 export default class DataTable extends Component {
@@ -40,22 +40,22 @@ export default class DataTable extends Component {
 		this.ref = null;
 	}
 
-	componentDidMount(){
+	componentDidMount() {
 		let {data, onRowClick, columns, id} = this.props;
-		data       = data ? data : [];
-		let _this = this;
-		this.table = new Tabulator(`#${id}`, {
-			height     : "40em",
-			movableRows: false,
-			selectable:true, //make rows selectable
-			columns:columns,
-			data:data,
+		data                                = data ? data : [];
+		let _this                           = this;
+		this.table                          = new Tabulator(`#${id}`, {
+			height             : "40em",
+			movableRows        : false,
+			selectable         : true, //make rows selectable
+			columns            : columns,
+			data               : data,
 			rowSelectionChanged: (data, rows) => {
-				for (let i = 0; i < rows.length - 1; i++){
+				for (let i = 0; i < rows.length - 1; i++) {
 					let row = rows[i];
 					row.deselect();
 				}
-				if (data.length > 0){
+				if (data.length > 0) {
 					_this.props.onRowClick(data[data.length - 1]);
 				}
 			},
@@ -65,13 +65,9 @@ export default class DataTable extends Component {
 	componentDidUpdate(prevProps, prevState, snapshot) {
 
 		let {data, onRowClick} = this.props;
-		data       = data ? data : [];
+		data                   = data ? data : [];
 
-		if (prevProps.data !== this.props.data){
-			console.log("componentDidUpdate componentDidUpdate componentDidUpdate");
-			console.log("componentDidUpdate componentDidUpdate componentDidUpdate");
-			console.log("componentDidUpdate componentDidUpdate componentDidUpdate");
-			console.log("tableData: ", data);
+		if (prevProps.data !== this.props.data) {
 			this.table.replaceData(data);
 		}
 	}
@@ -79,14 +75,14 @@ export default class DataTable extends Component {
 	render() {
 		let {columns, data, options, onRowClick, id} = this.props;
 		/*return (
-			<ReactTabulator
-				ref={ref => this.ref = ref}
-				columns={columns}
-				data={data}
-				options={options}
-				rowClick={onRowClick}
-			/>
-		);*/
+		 <ReactTabulator
+		 ref={ref => this.ref = ref}
+		 columns={columns}
+		 data={data}
+		 options={options}
+		 rowClick={onRowClick}
+		 />
+		 );*/
 		return (
 			<div id={id}></div>
 		);
